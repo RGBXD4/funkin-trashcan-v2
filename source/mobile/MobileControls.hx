3a0ebe8fad9e9c0c7640360bab37dc3dc652d9dd
8bit <139103755+RGBXD4@users.noreply.github.com>
2025-03-29 09:09:25 +0800

    Create MobileControls.hx

---
+++ source/mobile/MobileControls.hx
@@ -0,0 +1,32 @@
+package mobile;
+
+import flixel.FlxG;
+import flixel.group.FlxSpriteGroup;
+import flixel.math.FlxPoint;
+import flixel.util.FlxDestroyUtil;
+import mobile.flixel.FlxHitbox;
+
+/**
+ * @author Mihai Alexandru (M.A. Jigsaw)
+ */
+class MobileControls extends FlxSpriteGroup
+{
+
+	public var hitbox:FlxHitbox;
+
+	public function new()
+	{
+		super();
+
+				hitbox = new FlxHitbox();
+				add(hitbox);
+	}
+
+	override public function destroy():Void
+	{
+		super.destroy();
+
+		if (hitbox != null)
+			hitbox = FlxDestroyUtil.destroy(hitbox);
+	}
+}
