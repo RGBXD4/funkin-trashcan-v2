98dbd252e24d891b9e950d2b30ead0e0a0ca5446
8bit <139103755+RGBXD4@users.noreply.github.com>
2025-03-30 18:33:14 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -65,7 +65,8 @@ class Main extends Sprite
 	var game = {
 		width: 1280, // WINDOW width
 		height: 720, // WINDOW height
-		initialState: #if mobile mobile.states.CopyState #else TitleState #end, // initial game state
+		//initialState: #if mobile mobile.states.CopyState #else TitleState #end, // initial game state
+		initialState: TitleState, // initial game state
 		framerate: 60, // default framerate
 		skipSplash: true, // if the default flixel splash screen should be skipped
 		startFullscreen: false // if the game should start at fullscreen mode
